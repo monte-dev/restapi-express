185c996e77ded1ee19edfcfd14dfa8d50fcb74a9
monte-dev <badziobw@gmail.com>
2023-07-27 01:50:58 +0100

    Add tickets attribute to client side

--- client/src/components/features/Concert/Concert.js
+++ client/src/components/features/Concert/Concert.js
@@ -2,24 +2,37 @@ import { Row, Col } from 'reactstrap';
 
 import './Concert.scss';
 
-const Concert = ({ performer, price, genre, day, image }) => (
-  <article className="concert">
-    <Row noGutters>
-      <Col xs="6">
-        <div className="concert__image-container">
-          <img className="concert__image-container__img" src={image} alt={performer}/>
-        </div>
-      </Col>
-      <Col xs="6">
-        <div className="concert__info">
-          <img className="concert__info__back" src={image} alt={performer}/>
-          <h2 className="concert__info__performer">{ performer }</h2>
-          <h3 className="concert__info__genre">{ genre }</h3>
-          <p className="concert__info__day-n-price">Day: {day}, Price: { price }$</p>
-        </div>
-      </Col>
-    </Row>
-  </article>
+const Concert = ({ performer, price, genre, day, image, tickets }) => (
+	<article className="concert">
+		<Row noGutters>
+			<Col xs="6">
+				<div className="concert__image-container">
+					<img
+						className="concert__image-container__img"
+						src={image}
+						alt={performer}
+					/>
+				</div>
+			</Col>
+			<Col xs="6">
+				<div className="concert__info">
+					<img
+						className="concert__info__back"
+						src={image}
+						alt={performer}
+					/>
+					<h2 className="concert__info__performer">{performer}</h2>
+					<h3 className="concert__info__genre">{genre}</h3>
+					<p className="concert__info__tickets">
+						Only {tickets} tickets left.
+					</p>
+					<p className="concert__info__day-n-price">
+						Day: {day}, Price: {price}$
+					</p>
+				</div>
+			</Col>
+		</Row>
+	</article>
 );
 
 export default Concert;
